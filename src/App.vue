a3f28c0aa12df5c2deab61487200ed106dade08e
Min Mon Chel <minmonchel@gmail.com>
2025-06-17 10:52:23 +0700

    First commit - Nav bar and Hero section

---
+++ src/App.vue
@@ -0,0 +1,113 @@
+
+<!-- <template>
+  <div class="flex bg-orange-600 items-center justify-between px-10 py-2">
+    <div class="w-[50%]">
+      <img class="w-[30px]" src="https://99designs-blog.imgix.net/blog/wp-content/uploads/2022/06/Starbucks_Corporation_Logo_2011.svg-e1657703028844.png?auto=format&q=60&fit=max&w=930" alt="">
+    </div>
+    <div class="w-[50%] flex gap-7 text-white font-semibold items-center">
+      <ul class="flex gap-7">
+        <li>Home</li>
+        <li>Features</li>
+        <li>Portfolio</li>
+        <li>Blog</li>
+        <li>Pricing</li>
+      </ul>
+      <button class="bg-yellow-300 px-6 py-2 rounded-md">Get Started Now</button>
+    </div>
+  </div>
+</template> -->
+
+
+<!-- <template>
+  <div class="flex justify-between rounded-md items-center font-semibold bg-purple-400 px-6 py-2">
+    <div class="flex">
+      <img class="w-[30px] h-[30px] mr-2" src="https://99designs-blog.imgix.net/blog/wp-content/uploads/2022/06/Starbucks_Corporation_Logo_2011.svg-e1657703028844.png?auto=format&q=60&fit=max&w=930" alt="">
+      <h1>unplug ui</h1>
+    </div>
+    <div>
+      <ul class="flex gap-4">
+        <li>Home</li>
+        <li>Pages</li>
+        <li>Portfolio</li>
+        <li>Blog</li>
+        <li>Elements</li>
+      </ul>
+    </div>
+    <div>
+      <button class="px-6 py-2 rounded-lg bg-white text-purple-500">Search</button>
+    </div>
+  </div>
+</template> -->
+
+
+<!-- <template>
+  <div class="grid grid-cols-4 gap-2 grid-row-3 w-[700px] h-[400px] border-[1px] border-black-700">
+    <div class="bg-green-300 row-span-3"></div>
+    <div class="bg-green-300 col-span-2"></div>
+    <div class="bg-red-500"></div>
+    <div class="bg-orange-400"></div>
+    <div class="bg-red-500"></div>
+    <div class="bg-green-300 row-span-2"></div>
+    <div class="bg-red-500"></div>
+    <div class="bg-orange-400"></div>
+  </div>
+</template> -->
+
+
+<!-- <template>
+  <input type="email" class=" peer border-[1px] rounded-md mt-2 ml-2">
+  <p class="ml-2 invisible peer-invalid:visible">Unvalid Email Address</p>
+</template> -->
+
+
+<template>
+  <div class="bg-own-secondary">
+    <div class="flex justify-between items-center border-b-2 border-border xl:px-20 padding">
+      <div>
+        <img class="w-[70px]" src="https://cdn.prod.website-files.com/6627e9a3eba2a9009dbdeffe/6628074b71d6e1e95593d310_Logo.svg" alt="">
+      </div>
+      <div class="flex justify-between items-center gap-6">
+        <ul class="lg:flex hidden justify-between gap-6 text-lg cursor-pointer">
+          <li>Process</li>
+          <li>Integrations</li>
+          <li>Pricing</li>
+          <li>FAQ</li>
+          <li>Reviews</li>
+        </ul>
+        <button class="btn lg:flex hidden">Buy Teamplate</button>
+        <svg xmlns="http://www.w3.org/2000/svg" class="lg:hidden block" width="36" height="36" viewBox="0 0 24 24"><!-- Icon from Google Material Icons by Material Design Authors - https://github.com/material-icons/material-icons/blob/master/LICENSE --><path fill="currentColor" d="M3 18h18v-2H3zm0-5h18v-2H3zm0-7v2h18V6z"/></svg>
+      </div>
+    </div>
+
+    <div class="flex md:flex-row flex-col md:gap-0 gap-8 padding mt-20 bg-no-repeat hero-bg-img md:bg-[url(https://cdn.prod.website-files.com/6627e9a3eba2a9009dbdeffe/665b169cd354209f30160ac5_Square-Shapes.svg)]">
+      <div class="md:w-[50%] w-full">
+        <div class="xl:w-[100%] lg:w-full flex flex-col gap-6">
+          <h1 class="lg:text-6xl text-5xl leading-[1.2] font-bold">Landing Page Template for <span class="text-own-primary">Startups</span></h1>
+          <p class="text-lg">Use our template to quickly and efficiently brand your growing business.</p>
+          <div class="flex md:flex-row flex-col items-center gap-3">
+            <button class="btn md:basis-auto shrink-0 md:w-auto w-full !px-6 !py-4 text-lg">Preview Template</button>
+            <button class="outline-btn md:basis-auto shrink-0 md:w-auto w-full !px-6 !py-4 text-lg">Learn More</button>
+          </div>
+        </div>
+      </div>
+      <div class="md:w-[50%] w-full pb-20">
+        <img class="" src="https://cdn.prod.website-files.com/6627e9a3eba2a9009dbdeffe/662a5c31b674b313da821db0_Hero-Illustration.svg" alt="">
+      </div>
+    </div>
+
+    <div class="padding">
+      <h1 class="text-[22px] font-medium lg:text-center text-left">More than <span class="text-own-primary">1000+</span>Companies using our template</h1>
+      <div class="grid md:grid-cols-6 grid-cols-2 gap-8 items-center justify-items-center mt-4">
+        <img src="https://cdn.prod.website-files.com/6627e9a3eba2a9009dbdeffe/6665ab9ed663c4f53ebba1e8_DotSquare-Logo.svg" alt="">
+        <img src="https://cdn.prod.website-files.com/6627e9a3eba2a9009dbdeffe/6665abbcfc4dca184183e738_Wiggle-Logo.svg" alt="">
+        <img src="https://cdn.prod.website-files.com/6627e9a3eba2a9009dbdeffe/6665abe3bb1cc3289b9bfd5a_Sealife-Logo.svg" alt="">
+        <img src="https://cdn.prod.website-files.com/6627e9a3eba2a9009dbdeffe/6665ac001434b3a48f948b84_LoopMedia-Logo.svg" alt="">
+        <img src="https://cdn.prod.website-files.com/6627e9a3eba2a9009dbdeffe/6665ac172b9167ca66cce90e_Tricole45-Logo.svg" alt="">
+        <img src="https://cdn.prod.website-files.com/6627e9a3eba2a9009dbdeffe/6665ac32bb1cc3289b9c29a4_PlutoDelux-Logo.svg" alt="">
+      </div>
+    </div>
+
+  </div>
+</template>
+
+<style scoped></style>
---
+++ tailwind.config.js
@@ -0,0 +1,18 @@
+/** @type {import('tailwindcss').Config} */
+export default {
+  content: [
+    "./index.html",
+    "./src/**/*.{vue,js,ts,jsx,tsx}",
+  ],
+  theme: {
+    extend: {
+      colors : {
+        'own-primary' : "#7A71EB",
+        'own-secondary' : "#EEEEEE",
+        'border' : '#6E6E6ED'
+      },
+    },
+  },
+  plugins: [],
+}
+
